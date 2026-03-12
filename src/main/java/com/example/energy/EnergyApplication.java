13e0748b926b07bb05e32386e317c3d30cc597b4
kompi12 <nika.hoi3@gmail.com>
2026-03-12 11:01:32 +0100

    Remove code

--- src/main/java/com/example/energy/EnergyApplication.java
+++ src/main/java/com/example/energy/EnergyApplication.java
@@ -9,11 +9,11 @@ public class EnergyApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
-
-		dotenv.entries().forEach(e ->
-				System.setProperty(e.getKey(), e.getValue())
-		);
+//		Dotenv dotenv = Dotenv.load();
+//
+//		dotenv.entries().forEach(e ->
+//				System.setProperty(e.getKey(), e.getValue())
+//		);
 		SpringApplication.run(EnergyApplication.class, args);
 	}
 
